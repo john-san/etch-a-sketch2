c92fb6e10b58d95e421bf66b6444a81c8dafd7e7
John Nguyen <npjohn92@gmail.com>
2021-12-03 11:52:39 -0800

    add createRow

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const container = document.createElement('div');
+container.classList.add('container');
+
+appendChild(container, 'body');
+
+function appendChild(el, parentQuery) {
+  const parent = document.querySelector(`${parentQuery}`);
+  parent.appendChild(el);
+}
+
+function createCell() {
+  const cell = document.createElement('div');
+  addClass(cell, 'cell');
+  return cell;
+}
+
+function createRow(length) {
+  const rowContainer = document.createElement('div');
+  addClass(rowContainer, 'row');
+  for (i = 0; i < length; i++) {
+    const cell = createCell();
+    rowContainer.appendChild(cell);
+  }
+  
+  appendChild(rowContainer, '.container');
+}
+
+function createGrid(size) {
+  for (i = 0; i < size; i++) {
+    createRow(size);
+  }
+}
+
+function addClass(el, ...classNames) {
+  classNames.forEach((className) => {
+    el.classList.add(className)
+  });
+}
+
+function removeClass(el, ...classNames) {
+  classNames.forEach((className) => {
+    el.classList.remove(className)
+  });
+}
